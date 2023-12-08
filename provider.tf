9ff2b408dab6759161ecf78dbc9999c1bfc4c528
Salvador-Menjivar-S <77468192+Salvador-Menjivar-S@users.noreply.github.com>
2023-12-07 19:28:20 -0600

    new yaml.4

--- provider.tf
+++ provider.tf
@@ -1,7 +1,14 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs
+
+
 provider "google" {
+  credentials = jsondecode(var.gcp_sa_key)
   project = "smenjivar-dev"
   region  = "us-central1"
+}
+
+provider "github" {
+  token = var.gh_token
 }
 
 # https://www.terraform.io/language/settings/backends/gcs
@@ -17,3 +24,7 @@ terraform {
     }
   }
 }
+
+
+
+
