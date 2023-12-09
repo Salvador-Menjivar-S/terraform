b6bedd659656d37c3ec1edd4724ea808f0d4a6ce
Salvador-Menjivar-S <77468192+Salvador-Menjivar-S@users.noreply.github.com>
2023-12-09 17:28:25 -0600

    new update

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "gcp_sa_key" {
   description = "Service account key for GCP authentication"
   type        = string
+  default     = "C:\\Users\\salvador\\Downloads\\credentials08.json"
 }
